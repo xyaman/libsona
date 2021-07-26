d3044f00d66f53dadbd00c3d371ea108dd0a9773
Diego Barría <dbarriapaiva@gmail.com>
2021-07-25 23:56:30 -0400

    Better bars secondary color support

--- SonaBarsView.m
+++ SonaBarsView.m
@@ -18,8 +18,12 @@
 		bar.frame = CGRectMake(leftOffset + i * (self.pointWidth + self.pointSpacing), self.frame.size.height, self.pointWidth, 0);
 		bar.startPoint = CGPointMake(0.5, 1.0);
 		bar.endPoint = CGPointMake(0.5, 0.0);
-		bar.colors =  [NSArray arrayWithObjects:(id)[self.pointColor CGColor], (id)[self.pointSecondaryColor CGColor], nil];
-		bar.locations = @[[NSNumber numberWithFloat:0.0], [NSNumber numberWithFloat:0.7]];
+		// bar.colors =  [NSArray arrayWithObjects:(id)[self.pointColor CGColor], (id)[self.pointSecondaryColor CGColor], nil];
+		// bar.locations = @[[NSNumber numberWithFloat:0.0], [NSNumber numberWithFloat:0.7]];
+
+		bar.colors = [NSArray arrayWithObjects:(id)[self.pointColor CGColor], (id)[self.pointColor CGColor], nil, nil, nil];
+		bar.locations = @[[NSNumber numberWithFloat:0.0], [NSNumber numberWithFloat:0.6], [NSNumber numberWithFloat:0.6], [NSNumber numberWithFloat:1.0]];
+
 		bar.cornerRadius = self.pointRadius;
 		[self.layer addSublayer:bar];
 	}
@@ -96,7 +100,8 @@
 		struct CGColor *secondaryColor = self.pointSecondaryColor ? self.pointSecondaryColor.CGColor : nil;
 
 		dispatch_async(dispatch_get_main_queue(), ^{
-			bar.colors =  [NSArray arrayWithObjects:(__bridge id)primaryColor, (__bridge id)secondaryColor, nil];
+			// bar.colors =  [NSArray arrayWithObjects:(__bridge id)primaryColor, (__bridge id)secondaryColor, nil];
+			bar.colors =  [NSArray arrayWithObjects:(__bridge id)primaryColor, (__bridge id)primaryColor, (__bridge id)secondaryColor, (__bridge id)secondaryColor, nil];
 			bar.frame = CGRectMake(bar.frame.origin.x, self.frame.size.height, bar.frame.size.width, -fabs(heightMultiplier * self.frame.size.height));
 		});
 	}	
